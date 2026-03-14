adc1560585b0be4b417a78a0a0fa099b7cec7954
51hamu51 <hawks_2023@outlook.jp>
2026-03-15 00:49:16 +0900

    背景をランダムに表示

--- Assets/Scripts/BackGroundManager.cs
+++ Assets/Scripts/BackGroundManager.cs
@@ -1,41 +1,56 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BackGroundManager : MonoBehaviour
 {
     [SerializeField] private float scrollSpeed;
-    public GameObject backG1;
-    public GameObject backG2;
+    public Image[] backGs = new Image[3];
 
     private Vector3 startPos;
     private Vector3 prePos;
-    private bool isG1Head;
+    public int headNum;
+    [SerializeField] private Sprite[] backSprites;
+    public int[] spriteNums = new int[3];
+
     void Start()
     {
-        isG1Head = true;
-        startPos = backG1.transform.position;
-        prePos = backG2.transform.position;
+        headNum = 0;
+        startPos = backGs[1].transform.position;
+        prePos = backGs[2].transform.position;
+
+        backGs[0].sprite = backSprites[0];
+        spriteNums[0] = 0;
+        backGs[1].sprite = backSprites[0];
+        spriteNums[1] = 0;
+        backGs[2].sprite = backSprites[0];
+        spriteNums[2] = 0;
     }
 
     void Update()
     {
-        backG1.transform.position += Vector3.left * scrollSpeed * Time.deltaTime;
-        backG2.transform.position += Vector3.left * scrollSpeed * Time.deltaTime;
-
-        if (isG1Head)
+        for (int i = 0; i < backGs.Length; i++)
         {
-            if (backG2.transform.position.x <= startPos.x)
-            {
-                backG1.transform.position = prePos;
-                isG1Head = false;
-            }
+            backGs[i].transform.position += Vector3.left * scrollSpeed * Time.deltaTime;
         }
-        else
+
+        if (backGs[(headNum + 2) % 3].transform.position.x <= startPos.x)
         {
-            if (backG1.transform.position.x <= startPos.x)
-            {
-                backG2.transform.position = prePos;
-                isG1Head = true;
-            }
+            backGs[headNum].transform.position = prePos;
+            headNum = (headNum + 1) % 3;
+            EnvironmentChange();
         }
     }
+
+    /// <summary>
+    /// 背景画像変更
+    /// </summary>
+    private void EnvironmentChange()
+    {
+        int rand = Random.Range(0, 4);
+        backGs[(headNum + 2) % 3].sprite = backSprites[rand];
+        spriteNums[(headNum + 2) % 3] = rand;
+    }
 }
+
+
+//spriteNums[(headNum + 1) % 3]が現在のプレイヤーが見えている景色
